94712fae24e7281a8d4cec8ef066a55fbe4eacd9
zwug <zwugme@gmail.com>
2018-06-18 22:30:10 +0300

    small fixes

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 
-const roundSeconds = [30, 20, 3]; // seconds for each round
+const roundSeconds = [30, 20, 15]; // seconds for each round
 
 const styles = StyleSheet.create({
   container: {
